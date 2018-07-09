7e10168ecb2e39266e588d24c5984c02388ff782
olivier <olivier.winter@logimethods.com>
2018-07-09 14:05:36 -0400

    setup_dev changes

--- Infrastructure/bin/setup_dev.sh
+++ Infrastructure/bin/setup_dev.sh
@@ -13,23 +13,6 @@ echo "Setting up Parks Development Environment in project ${GUID}-parks-dev"
 # Code to set up the parks development project.
 
 # To be Implemented by Student
-#Deploy front end
-
-#Build parks-map binary with maven
-pushd ../../ParksMap
-mvn -s ../nexus_settings.xml clean package spring-boot:repackage -DskipTests -Dcom.redhat.xpaas.repo.redhatga
-popd
-
-#Allow application permission to discover available routes
-oc policy add-role-to-user view --serviceaccount=default
-
-#Create build for parks-map and deploy
-oc new-build --binary=true --name=parksmap-dev --image-stream=redhat-openjdk18-openshift:1.2
-oc start-build parksmap-dev --from-file=../../ParksMap/target/parksmap.jar --follow
-oc new-app parksmap-dev -l type=parksmap-frontend -e APPNAME="ParksMap (Blue)"
-oc expose svc/parksmap-dev --port=8080
-oc set probe dc/parksmap-dev --readiness --get-url=http://:8080/ws/healthz/ --initial-delay-seconds=2
-oc set probe dc/parksmap-dev --liveness --get-url=http://:8080/ws/healthz/ --initial-delay-seconds=2
 
 
 ##Deploy mongo 
@@ -54,46 +37,24 @@ oc new-app -f ../templates/mongodb_persistent.json --param MEMORY_LIMIT=$MEMORY_
 --param MONGODB_VERSION=${MONGODB_VERSION} 
 
 
-##Deploy back ends (TODO)
+#Deploy front end
+oc new-build --binary=true --name="mlbparks" jboss-eap70-openshift:1.7 -n ${GUID}-parks-dev
+oc new-app ${GUID}-parks-dev/mlbparks:0.0-0 --name=mlbparks --allow-missing-imagestream-tags=true -n ${GUID}-parks-dev
+oc set triggers dc/mlbparks --remove-all -n ${GUID}-parks-dev
+oc expose dc mlbparks --port 8080 -n ${GUID}-parks-dev
+oc expose svc mlbparks -n ${GUID}-parks-dev
 
-#Build and Deploy National Parks
+echo "Creating Nationalparks base app"
+oc new-build --binary=true --name="nationalparks" redhat-openjdk18-openshift:1.2 -n ${GUID}-parks-dev
+oc new-app ${GUID}-parks-dev/nationalparks:0.0-0 --name=nationalparks --allow-missing-imagestream-tags=true -n ${GUID}-parks-dev
+oc set triggers dc/nationalparks --remove-all -n ${GUID}-parks-dev
+oc expose dc nationalparks -n ${GUID}-parks-dev
+oc expose svc nationalparks -n ${GUID}-tasks-dev
 
-#Build national-parks binary with maven
-pushd ../../Nationalparks
-mvn -s ../nexus_settings.xml clean package -DskipTests=true
-popd
-
-oc new-build --binary=true --name=national-parks-dev --image-stream=redhat-openjdk18-openshift:1.2
-oc start-build national-parks-dev --from-file=../../Nationalparks/target/nationalparks.jar --follow
-oc new-app national-parks-dev -l type=parksmap-backend -e APPNAME="National Parks (Blue)" \
--e DB_HOST=$DATABASE_SERVICE_NAME \
--e DB_PORT=27017 \
--e DB_USERNAME=$MONGODB_USER \
--e DB_PASSWORD=$MONGODB_PASSWORD \
--e DB_NAME=$MONGODB_DATABASE
-
-oc set probe dc/national-parks-dev --readiness --get-url=http://:8080/ws/healthz/ --initial-delay-seconds=30
-oc set probe dc/national-parks-dev --liveness --get-url=http://:8080/ws/healthz/ --initial-delay-seconds=30
-
-#Add post deployment hook!
-
-#Build and Deploy MLB Parks
-
-#Build mlb-parks binary with maven
-pushd ../../MLBParks
-mvn -s ../nexus_settings.xml clean package -DskipTests=true
-popd
-
-oc new-build --binary=true --name=mlb-parks-dev --image-stream=jboss-eap70-openshift:1.7
-oc start-build mlb-parks-dev --from-file=../../MLBParks/target/mlbparks.war --follow
-oc new-app mlb-parks-dev -l type=parksmap-backend -e APPNAME="MLB Parks (Blue)" \
--e DB_HOST=$DATABASE_SERVICE_NAME \
--e DB_PORT=27017 \
--e DB_USERNAME=$MONGODB_USER \
--e DB_PASSWORD=$MONGODB_PASSWORD \
--e DB_NAME=$MONGODB_DATABASE
-
-oc set probe dc/mlb-parks-dev --readiness --get-url=http://:8080/ws/healthz/ --initial-delay-seconds=30
-oc set probe dc/mlb-parks-dev --liveness --get-url=http://:8080/ws/healthz/ --initial-delay-seconds=30
-
-#Add post deployment hook!
+echo "Creating ParksMap base app"
+oc new-build --binary=true --name="parksmap" redhat-openjdk18-openshift:1.2 -n ${GUID}-parks-dev
+oc new-app ${GUID}-parks-dev/parksmap:0.0-0 --name=parksmap --allow-missing-imagestream-tags=true -n ${GUID}-parks-dev
+oc set triggers dc/parksmap --remove-all -n ${GUID}-parks-dev
+oc expose dc parksmap -n ${GUID}-parks-dev
+oc expose svc parksmap -n ${GUID}-parks-dev
+oc policy add-role-to-user view --serviceaccount=default -n ${GUID}-parks-dev
