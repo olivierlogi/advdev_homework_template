5dd8dee74e410262dd4756332b78632a6cf2ca1b
olivier <olivier.winter@logimethods.com>
2018-07-09 14:16:02 -0400

    add -n jenkins

--- Infrastructure/bin/setup_jenkins.sh
+++ Infrastructure/bin/setup_jenkins.sh
@@ -27,7 +27,7 @@ echo "Setting up Jenkins in project ${GUID}-jenkins from Git Repo ${REPO} for Cl
 # # * CLUSTER: the base url of the cluster used (e.g. na39.openshift.opentlc.com)
 
 # # To be Implemented by Student
-oc new-app -f ./Infrastructure/templates/jenkins.json -p MEMORY_LIMIT=2Gi
+oc new-app -f ./Infrastructure/templates/jenkins.json -p MEMORY_LIMIT=2Gi -n ${GUID}-jenkins
 
 #docker build ../docker -t docker-registry-default.apps.${CLUSTER}/${GUID}-jenkins/jenkins-slave-maven-appdev:v3.9
 
