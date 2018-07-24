2c8bb9896c86970fb5abb859ec8f076d5c34cccf
olivier <olivier.winter@logimethods.com>
2018-07-24 14:32:11 -0400

    setupdev changes

--- Infrastructure/bin/setup_dev.sh
+++ Infrastructure/bin/setup_dev.sh
@@ -9,6 +9,11 @@ fi
 GUID=$1
 echo "Setting up Parks Development Environment in project ${GUID}-parks-dev"
 
+##DAdd policy role
+oc policy add-role-to-user view --serviceaccount=default -n $GUID-parks-dev
+oc policy add-role-to-user edit system:serviceaccount:$GUID-jenkins:jenkins -n $GUID-parks-dev
+
+
 ##Deploy mongo
 echo "Deploy Mongo" 
 MEMORY_LIMIT="512Mi"
@@ -35,22 +40,25 @@ oc new-app -f ./Infrastructure/templates/mongodb_persistent.json -n ${GUID}-park
 oc new-build --binary=true --name="mlbparks" jboss-eap70-openshift:1.7 -n ${GUID}-parks-dev
 oc new-app ${GUID}-parks-dev/mlbparks:0.0-0 --name=mlbparks --allow-missing-imagestream-tags=true -n ${GUID}-parks-dev
 oc set triggers dc/mlbparks --remove-all -n ${GUID}-parks-dev
-oc expose dc mlbparks --port 8080 -n ${GUID}-parks-dev
+oc expose dc/mlbparks --port 8080 -n ${GUID}-parks-dev
 oc expose svc mlbparks -n ${GUID}-parks-dev
+oc create route edge mlbparks --service=mlbparks --port=8080 -n $GUID-parks-dev
 
 echo "Creating Nationalparks base app"
 oc new-build --binary=true --name="nationalparks" redhat-openjdk18-openshift:1.2 -n ${GUID}-parks-dev
 oc new-app ${GUID}-parks-dev/nationalparks:0.0-0 --name=nationalparks --allow-missing-imagestream-tags=true -n ${GUID}-parks-dev
 oc set triggers dc/nationalparks --remove-all -n ${GUID}-parks-dev
-oc expose dc nationalparks -n ${GUID}-parks-dev
+oc expose dc/nationalparks -n ${GUID}-parks-dev
 oc expose svc nationalparks -n ${GUID}-tasks-dev
+oc create route edge nationalparks --service=nationalparks --port=8080 -n $GUID-parks-dev
 
 echo "Creating ParksMap base app"
 oc new-build --binary=true --name="parksmap" redhat-openjdk18-openshift:1.2 -n ${GUID}-parks-dev
 oc new-app ${GUID}-parks-dev/parksmap:0.0-0 --name=parksmap --allow-missing-imagestream-tags=true -n ${GUID}-parks-dev
 oc set triggers dc/parksmap --remove-all -n ${GUID}-parks-dev
-oc expose dc parksmap -n ${GUID}-parks-dev
+oc expose dc/parksmap --port=8080 -n ${GUID}-parks-dev
 oc expose svc parksmap -n ${GUID}-parks-dev
-oc policy add-role-to-user view --serviceaccount=default -n ${GUID}-parks-dev
+oc create route edge parksmap --service=parksmap --port=8080 -n $GUID-parks-dev
+
 
 
