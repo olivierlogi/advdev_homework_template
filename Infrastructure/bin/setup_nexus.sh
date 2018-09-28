97667ee8ecde70400203b7cf04df21824ccc6089
olivier <olivier.winter@logimethods.com>
2018-09-28 14:28:07 -0400

    nexus changes

--- Infrastructure/bin/setup_nexus.sh
+++ Infrastructure/bin/setup_nexus.sh
@@ -21,28 +21,30 @@ oc project $GUID-nexus
 # Ideally just calls a template
 # oc new-app -f ../templates/nexus.yaml --param .....
 
-PROJECT_NAME=$GUID-nexus
-
 
 ################>>>>>>>>>>>>>>>replace with template
 
 
 oc new-app -f ./Infrastructure/templates/nexus_template.yaml -n "${GUID}-nexus"
 ##########################################<<<<<<<<<<<<<<<<replace with template
+echo -n "Checking if Nexus is Ready..."
 while : ; do
-    echo "Checking if Nexus is Ready..."
-    oc get pod -n ${GUID}-nexus | grep -v deploy|grep "1/1"
-    [[ "$?" == "1" ]] || break
-    echo "...no. Sleeping 10 seconds."
-    sleep 10
+##  oc get pod -n ${GUID}-nexus|grep '\-2\-'|grep -v deploy|grep "1/1"
+  oc get pod -n ${GUID}-nexus | grep -v deploy|grep "1/1"
+  [[ "$?" == "1" ]] || break
+  echo -n "."
+  sleep 10
 done
 
 echo -n "Configuring Nexus"
+# https://www.opentlc.com/labs/ocp_advanced_development/04_1_CICD_Tools_Solution_Lab.html#_configure_nexus
 
 curl -o setup_nexus3.sh -s https://raw.githubusercontent.com/wkulhanek/ocp_advanced_development_resources/master/nexus/setup_nexus3.sh
 chmod +x setup_nexus3.sh
 ./setup_nexus3.sh admin admin123 http://$(oc get route nexus3 --template='{{ .spec.host }}' -n "${GUID}-nexus")
 rm setup_nexus3.sh
+
+echo -n "Configuring Nexus Service and Route"
 
 oc expose dc nexus3 --port=5000 --name=nexus-registry -n "${GUID}-nexus"
 oc create route edge nexus-registry --service=nexus-registry --port=5000 -n "${GUID}-nexus"
