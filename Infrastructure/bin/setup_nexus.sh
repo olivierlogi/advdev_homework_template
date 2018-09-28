59798cf3b37008e421d12c199548aa5a84e99218
olivier <olivier.winter@logimethods.com>
2018-09-28 13:37:11 -0400

    nexus changes

--- Infrastructure/bin/setup_nexus.sh
+++ Infrastructure/bin/setup_nexus.sh
@@ -39,8 +39,8 @@ done
 
 curl -o setup_nexus3.sh -s https://raw.githubusercontent.com/wkulhanek/ocp_advanced_development_resources/master/nexus/setup_nexus3.sh
 chmod +x setup_nexus3.sh
-./setup_nexus3.sh admin admin123 http://$(oc get route nexus --template='{{ .spec.host }}')
+./setup_nexus3.sh admin admin123 http://$(oc get route nexus3 --template='{{ .spec.host }}' -n "${GUID}-nexus")
 rm setup_nexus3.sh
 
-oc expose dc nexus3 --port=5000 --name=nexus-registry
-oc create route edge nexus-registry --service=nexus-registry --port=5000
+oc expose dc nexus3 --port=5000 --name=nexus-registry -n "${GUID}-nexus"
+oc create route edge nexus-registry --service=nexus-registry --port=5000 -n "${GUID}-nexus"
