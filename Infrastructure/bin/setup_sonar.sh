70c909f5db27972c5fc78d1e4123092357128562
olivier <olivier.winter@logimethods.com>
2018-10-01 11:09:22 -0400

    sonar changes

--- Infrastructure/bin/setup_sonar.sh
+++ Infrastructure/bin/setup_sonar.sh
@@ -11,4 +11,4 @@ echo "Setting up Sonarqube in project $GUID-sonarqube"
 
 # Code to set up the SonarQube project.
 # Ideally just calls a template
-oc new-app -f ./Infrastructure/templates/sonar_template.yaml --param GUID=${GUID} POSTGRESQL_USER=sonar --param POSTGRESQL_PASSWORD=sonar --param POSTGRESQL_DATABASE=sonar --param POSTGRESQL_VOLUME_CAPACITY=4Gi -n $GUID-sonarqube
+oc new-app -f ./Infrastructure/templates/sonar_template.yaml --param=SONARQUBE_IMAGE=docker.io/wkulhanek/sonarqube --param=SONARQUBE_VERSION=7.3 -n $GUID-sonarqube
