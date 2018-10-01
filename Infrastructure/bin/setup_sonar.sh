ca4e801404b23ec7b4fba41a804645caca53c3d8
olivier <olivier.winter@logimethods.com>
2018-10-01 15:03:27 -0400

    sonar changes

--- Infrastructure/bin/setup_sonar.sh
+++ Infrastructure/bin/setup_sonar.sh
@@ -9,6 +9,10 @@ fi
 GUID=$1
 echo "Setting up Sonarqube in project $GUID-sonarqube"
 
+oc policy add-role-to-user admin system:serviceaccount:gpte-jenkins:jenkins -n ${GUID}-sonarqube
+oc policy add-role-to-user view system:serviceaccount:gpte-jenkins:jenkins -n ${GUID}-sonarqube
+oc policy add-role-to-user edit system:serviceaccount:gpte-jenkins:jenkins -n ${GUID}-sonarqube
+
 # Code to set up the SonarQube project.
 # Ideally just calls a template
-oc new-app -f ./Infrastructure/templates/sonar_template.yaml --param=SONARQUBE_IMAGE=docker.io/wkulhanek/sonarqube --param=SONARQUBE_VERSION=7.3 -n $GUID-sonarqube
+oc new-app -f ./Infrastructure/templates/sonar_template.yaml -p GUID=${GUID} -n ${GUID}-sonarqube
