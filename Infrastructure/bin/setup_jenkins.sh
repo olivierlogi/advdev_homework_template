b55cd187bccfd27c0e4748e7812cf3e5b5b53eb4
olivier <olivier.winter@logimethods.com>
2018-10-10 20:50:49 -0400

    jenkins changes

--- Infrastructure/bin/setup_jenkins.sh
+++ Infrastructure/bin/setup_jenkins.sh
@@ -34,7 +34,7 @@ oc policy add-role-to-user edit system:serviceaccount:$GUID-jenkins:jenkins -n $
 oc new-app -f ./Infrastructure/templates/jenkins.json \
   --param ENABLE_OAUTH=true \
   --param MEMORY_LIMIT=2Gi \
-  --param CPU_LIMIT=1 \
+  --param CPU_LIMIT=2 \
   --param VOLUME_CAPACITY=4Gi \
   -n "${GUID}-jenkins"
 
